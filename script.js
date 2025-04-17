7b934349a68150db483596c934df50da4d82e960
Jarronadam <Jarrondouglas@gmail.com>
2025-04-16 23:40:30 -0400

    Bulk of front end is done, still need to fix nav bar and project containers

--- script.js
+++ script.js
@@ -5,3 +5,32 @@ function toggleMenu() {
     menu.classList.toggle('open'); // This will add or remove the 'open' class
     icon.classList.toggle('open'); // This will add or remove the 'open' class
 }
+
+const titles = [
+    "Software Developer",
+    "IT Technician",
+    "Full-Stack Engineer",
+    "Web Developer",
+    "Problem Solver"
+  ];
+
+  let index = 0;
+const textElement = document.getElementById("changing-text");
+
+function changeTitle() {
+    // Fade out
+    textElement.classList.remove("fade-in");
+    textElement.classList.add("fade-out");
+  
+    setTimeout(() => {
+      index = (index + 1) % titles.length;
+      textElement.textContent = titles[index];
+  
+      // Fade back in
+      textElement.classList.remove("fade-out");
+      textElement.classList.add("fade-in");
+    }, 400); // wait for fade out before switching
+  }
+  
+  // Change every 2.5s
+  setInterval(changeTitle, 2500);
